5ade1a6ffbfd46664b0c30bdf1300cf61cbf7b93
DaniloMV <danilo7valladarez@gmail.com>
2021-04-19 17:18:52 -0500

    Cambio de Nombre EntidadSistema a ObjetoEscuelaBase

---
+++ Entidades/ObjetoEscuelaBase.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace CoreEscuela.Entidades
+{
+    public class ObjetoEscuelaBase
+    {
+        //public string UniqueId { get; protected internal set; }
+        public string UniqueId { get; private set; }
+        public string Nombre { get; set; }
+
+        public ObjetoEscuelaBase()
+        {
+            UniqueId = Guid.NewGuid().ToString();
+        }
+
+        public override string ToString()
+        {
+            return $"{Nombre}, {UniqueId}";
+        }
+    }
+}
